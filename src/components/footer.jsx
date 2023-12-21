4ee40440d79cda270ae81213d37e9d6e3de8447c
iamnoufal <jnrahman12@gmail.com>
2023-12-21 17:04:33 +0530

    chota chota fixes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -12,9 +12,9 @@ const Footer = () => {
         <Box sx={{display: 'flex', justifyContent:{xs:"center",md:'flex-start'}, pb:2}}>
           <Link style={{textDecoration: 'none'}} href="/"><Typography className='ms' sx={{color: "white",fontSize: 12}}>Home</Typography></Link>
           <Link style={{textDecoration: 'none'}} href="/blog"><Typography className='ms' sx={{color: "white",fontSize: 12, mx: 2}}>Blog</Typography></Link>
-          <Link style={{textDecoration: 'none'}} href="mailto:jnrahman12@gmail.com"><Typography className='ms' sx={{color: "white",fontSize: 12}}>Mail</Typography></Link>
+          <Link style={{textDecoration: 'none'}} href="mailto:iam@noufal.dev"><Typography className='ms' sx={{color: "white",fontSize: 12}}>Mail</Typography></Link>
         </Box>
-        <Typography fontSize={12} color="white" textAlign="center" className="ms" pb={2}>&copy; {new Date().getFullYear()} All rights reserved | v2.0</Typography>
+        <Typography fontSize={12} color="white" textAlign="center" className="ms" pb={2}>&copy; {new Date().getFullYear()} All rights reserved | v3.0</Typography>
       </Container>
     </Box>
   )
